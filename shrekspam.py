61f928e9f31d3f7e5f55094b981007eccd92b9e6
Basic-SkriptKiddie <73158446+Basic-SkriptKiddie@users.noreply.github.com>
2021-03-13 03:22:24 -0600

    Update shrekspam.py

--- shrekspam.py
+++ shrekspam.py
@@ -1,11 +1,16 @@
 import pyautogui
 import time
 import os
+# clearing shell
 os.system('cls')
-print("starting spam from spam.txt")
+print("starting spam")
+# waits before spaming
+os.system('ping localhost -n 5 > nul')
 time.sleep(5)
+# reads content from a txt file
 f = open("shrek.txt", 'r')
 for word in f:
+#   enters the script and sends it
     pyautogui.typewrite(word)
     pyautogui.press("enter")
     
